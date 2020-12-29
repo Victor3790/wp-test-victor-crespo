fc2400909f2cd3306ef90f566dfec91826a5092e
Victor <victor3790@gmail.com>
2020-12-29 17:47:19 -0600

    Redirection fixed for test

--- web/app/plugins/the-pet-club/includes/class-tpc.php
+++ web/app/plugins/the-pet-club/includes/class-tpc.php
@@ -187,8 +187,8 @@ class Tpc {
         $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 		$this->loader->add_action( 'init', $plugin_public, 'register_shortcodes' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_media_uploader' );
-		$this->loader->add_action( 'template_redirect', $plugin_public, 'check_registration' );
-		$this->loader->add_action( 'elementor/query/tpc_keepers', $search, 'search' );
+		//$this->loader->add_action( 'template_redirect', $plugin_public, 'check_registration' );
+		//$this->loader->add_action( 'elementor/query/tpc_keepers', $search, 'search' );
 		$this->loader->add_action( 'rest_api_init', $mercado_pago, 'register_route' );
 
 		$this->loader->add_filter( 'dokan_get_dashboard_nav', $plugin_public, 'modify_dokan_dashboard' );
