dc99b65edc40c1dd7c5e650706c6ac857e2ac0e3
Victor <victor3790@gmail.com>
2020-12-30 23:45:42 -0600

    Profile added

--- web/app/plugins/the-pet-club/public/includes/class_mercado_pago.php
+++ web/app/plugins/the-pet-club/public/includes/class_mercado_pago.php
@@ -12,8 +12,6 @@ if(!class_exists('Tpc_Mercado_Pago'))
         {
             register_rest_route( 'tpc/v1', '/subscription', array(
                 'methods' => WP_REST_SERVER::CREATABLE,
-                /*'methods' => 'POST',*/
-                /*'callback' => function() {return new WP_REST_Response( [ 'status'=>200 ] );},*/
                 'args' => array(),
                 'callback' => array( $this, 'get_response' )
               ) 
@@ -26,8 +24,6 @@ if(!class_exists('Tpc_Mercado_Pago'))
             $actual = file_get_contents($fichero);
             $actual .= var_dump($json);
             file_put_contents($fichero, $actual);
-
-            trigger_error( 'Hey!!!!!!!!   ..................' );
 
             return new WP_REST_Response( [ 'status'=>200 ] );
         }
