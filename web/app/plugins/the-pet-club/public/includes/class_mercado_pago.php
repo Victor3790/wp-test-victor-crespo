8a9eadcbbe4855f56085fc5bda8d2e23971121a4
Victor <victor3790@gmail.com>
2020-12-30 20:33:21 -0600

    No txt file

--- web/app/plugins/the-pet-club/public/includes/class_mercado_pago.php
+++ web/app/plugins/the-pet-club/public/includes/class_mercado_pago.php
@@ -19,10 +19,10 @@ if(!class_exists('Tpc_Mercado_Pago'))
 
         function get_response( $json )
         {
-	        $fichero = 'payments.txt';
+	        /*$fichero = 'payments.txt';
             $actual = file_get_contents($fichero);
             $actual .= $json;
-            file_put_contents($fichero, $actual);
+            file_put_contents($fichero, $actual);*/
 
             return new WP_REST_Response( [ 'status'=>200 ] );
         }
